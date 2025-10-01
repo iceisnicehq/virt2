c266888f797012d1d3273485cb304bdbc0d26a2b
Sab <66420569+iceisnicehq@users.noreply.github.com>
2025-10-01 19:56:39 +0300

    Create 1.ps1

---
+++ 1.ps1
@@ -0,0 +1,54 @@
+# 2.1.ps1 - Создание виртуальных машин
+
+# Имена ВМ для создания
+$VMNames = @("VM1", "VM2", "VM3", "VM4", "VM5") 
+$Memory = 4096MB # Объем ОЗУ
+$VHDSize = 52GB # Размер виртуального диска
+$PathToISO = "C:\VMs_ISO\WinServ2019.iso" # Путь к установочному образу ОС
+$Gen = 2 # Поколение ВМ
+$Switch1 = "10.9.12.0/24" # Имя первого коммутатора
+$Switch2 = "10.9.19.0/24" # Имя второго коммутатора
+
+# Создание виртуальных коммутаторов
+New-VMSwitch -Name $Switch1 -SwitchType Private -Notes "Подсеть 1"
+New-VMSwitch -Name $Switch2 -SwitchType Private -Notes "Подсеть 2"
+
+# Цикл для создания каждой ВМ
+foreach ($VMName in $VMNames) {
+    $VHDPath = "C:\VM\$VMName.vhdx"
+    
+    # Создание ВМ
+    Write-Host "Создание виртуальной машины: $VMName"
+    New-VM -Name $VMName -MemoryStartupBytes $Memory -Generation $Gen -NewVHDPath $VHDPath -NewVHDSizeBytes $VHDSize
+    
+    # Настройка параметров ВМ
+    Set-VM -Name $VMName -EnhancedSessionTransportType HVSocket
+    Set-VMFirmware -VMName $VMName -EnableSecureBoot Off
+    Set-VMProcessor -VMName $VMName -Count 4
+    
+    # Подключение ISO-образа
+    Add-VMDvdDrive -VMName $VMName -Path $PathToISO
+    
+    # Запуск ВМ для установки ОС
+    Start-VM -Name $VMName
+    
+    VMConnect.exe localhost $VMName # Раскомментируйте, чтобы открыть консоль
+}
+
+# После установки ОС на всех машинах, подключите их к коммутаторам
+Write-Host "Подключение сетевых адаптеров к коммутаторам..."
+Add-VMNetworkAdapter -VMName "VM1" -SwitchName $Switch1
+Add-VMNetworkAdapter -VMName "VM2" -SwitchName $Switch1
+Add-VMNetworkAdapter -VMName "VM5" -SwitchName $Switch1
+
+Add-VMNetworkAdapter -VMName "VM1" -SwitchName $Switch2
+Add-VMNetworkAdapter -VMName "VM3" -SwitchName $Switch2
+Add-VMNetworkAdapter -VMName "VM4" -SwitchName $Switch2
+
+# Подключение VM3 к внешней сети
+$ExternalSwitch = Get-VMSwitch -SwitchType External
+if ($ExternalSwitch) {
+    Add-VMNetworkAdapter -VMName "VM3" -SwitchName $ExternalSwitch.Name
+} else {
+    Write-Warning "Внешний коммутатор не найден. VM3 не будет иметь доступа к внешней сети."
+}
