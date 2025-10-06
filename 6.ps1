347dd8287bbeb52b8bea0d1097fc2e5521f610cc
iceisnicehq <iceisnicehq@gmail.com>
2025-10-06 19:21:16 +0300

    vars

--- 6.ps1
+++ 6.ps1
@@ -1,17 +1,12 @@
 # 6.1.ps1 - Работа с контрольными точками на VM3
+Checkpoint-VM -Name "VM4" -SnapshotName "before_update"
 
-$VMName = "VM4"
-$CheckpointName1 = "before_update"
-$CheckpointName2 = "after_update"
+# Cоздание файла для проверки..... руками
 
-Checkpoint-VM -Name $VMName -SnapshotName $CheckpointName1
+Checkpoint-VM -Name "VM4" -SnapshotName "after_update"
 
-# Cоздание файла для проверки
+Restore-VMCheckpoint -Name "VM4" -Name "before_update" -Confirm:$false
+Start-VM -Name "VM4"
 
-Checkpoint-VM -Name $VMName -SnapshotName $CheckpointName2
-
-Restore-VMCheckpoint -Name $VMName -Name $CheckpointName1 -Confirm:$false
-Start-VM -Name $VMName
-
-Restore-VMCheckpoint -Name $VMName -Name $CheckpointName2 -Confirm:$false
-Start-VM -Name $VMName
+Restore-VMCheckpoint -Name "VM4" -Name "after_update" -Confirm:$false
+Start-VM -Name "VM4"
--- 8.ps1
+++ 8.ps1
@@ -1,11 +1,8 @@
 # 8.1.ps1 - Перенос узлов в другую подсеть
 
-$SourceSwitchName = "10.9.12.0/24"
-$TargetSubnet = "10.9.10.0" 
-$TargetSwitchName = "$TargetSubnet/24"
-$ExportPath = "C:\MigrationExport"
-
 New-VMSwitch -Name "10.9.10.0/24" -SwitchType Private
+
+# Импорт экспорт как в 5.0.ps1
 
 # и дальше всё как в 3.0.ps1
 # Удаляем лишние адаптеры
